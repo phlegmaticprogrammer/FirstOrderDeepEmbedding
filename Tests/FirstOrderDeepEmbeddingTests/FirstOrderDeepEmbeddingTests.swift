bd531cda9c91b2556df51ada725fde7af1e10839
Steven Obua <steven@recursivemind.com>
2020-04-10 17:31:59 +0100

    Update FirstOrderDeepEmbeddingTests.swift

--- Tests/FirstOrderDeepEmbeddingTests/FirstOrderDeepEmbeddingTests.swift
+++ Tests/FirstOrderDeepEmbeddingTests/FirstOrderDeepEmbeddingTests.swift
@@ -284,7 +284,7 @@ final class FirstOrderDeepEmbeddingTests: XCTestCase {
             return name as? Int
         }
         
-        eval(typeEnv: typeEnv, env: env, INT.Var(5) + INT.Var(7), result: 12)
+        eval(typeEnv: typeEnv, env: env, INT.Var(5) + INT.Var(7), result: 11)
     }
     
     func testTermStore() {
