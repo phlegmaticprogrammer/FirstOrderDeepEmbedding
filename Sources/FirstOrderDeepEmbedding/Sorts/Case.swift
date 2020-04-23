383ea7c553d55430a9947575e9c521c6d1eaba75
Steven Obua <steven@recursivemind.com>
2020-04-24 00:54:34 +0100

    Update Case.swift

--- Sources/FirstOrderDeepEmbedding/Sorts/Case.swift
+++ Sources/FirstOrderDeepEmbedding/Sorts/Case.swift
@@ -2,7 +2,13 @@ infix operator => : AssignmentPrecedence
 
 public struct Case<Pattern, Result> {
     public let pattern : Pattern
+    
     public let result : Result
+    
+    public init(pattern : Pattern, result : Result) {
+        self.pattern = pattern
+        self.result = result
+    }
 }
 
 public func => <Pattern, Result>(pattern : Pattern, result : Result) -> Case<Pattern, Result> {
